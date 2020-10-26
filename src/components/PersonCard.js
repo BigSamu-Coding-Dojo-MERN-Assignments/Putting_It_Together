cff150343b8a129f06479b59ae22da56bf8552ae
Samuel <samuel@MacBook-Pro-de-Samuel.local>
2020-10-26 18:32:54 +0000

    Submitting 'Put It Together' Assignmnet v2

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
     
 class PersonCard extends Component {
     constructor(props){
-        super(props)
+        super(props);
         const {firstName, lastName, age, hairColor} = props;
         this.state = {
             firstName: firstName,
@@ -15,6 +15,7 @@ class PersonCard extends Component {
 
     increaseAge = () =>{
         this.setState({ age: this.state.age + 1 });
+        console.log("Increasing Age!")
     }
 
     render() {
@@ -24,7 +25,7 @@ class PersonCard extends Component {
                <h1> {this.state.lastName}, {this.state.firstName} </h1>
                <p> Age: {this.state.age} </p>
                <p> Hair Color: {this.state.hairColor} </p>
-               <button type="button" className= "btn btn-success " onClick= {this.changeAge} >Birthday Button for{ this.state.firstName} {this.state.lastName} </button>
+               <button type="button" className= "btn btn-success " onClick= {this.increaseAge}> Birthday Button for { this.state.firstName} {this.state.lastName} </button>
             </div>
         </div>
         
